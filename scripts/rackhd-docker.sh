4fc7f22503bfad2440ddbfbf75884bbb42c36a33
davlloyd <david.lloyd@emc.com>
2016-08-19 08:21:50 +1000

    Updated RackHD Docker installation

--- scripts/rackhd-docker.sh
+++ scripts/rackhd-docker.sh
@@ -5,10 +5,28 @@
 . /vagrant/scripts/color.sh
 
 logger "blue" "Installing Docker"
+# Install Docker prereqs
+sudo apt-get -y install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get -y install linux-image-extra-$(uname -r)
+
+# Add docker repo to sources
 sudo su
-curl -L https://get.docker.com/builds/Linux/x86_64/docker-1.11.0.tgz > docker-1.11.0.tgz && tar -xf docker-1.11.0.tgz && cp docker/docker /usr/local/bin/docker && chmod +x /usr/local/bin/docker
-curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
+sudo echo deb https://apt.dockerproject.org/repo ubuntu-trusty main >/etc/apt/sources.list.d/docker.list
+sudo apt-get -y update
+
+# Install docker
+sudo apt-get -y install docker-engine
+
+# Get the service running and configured
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+sudo service docker start
+
+# Docker machine installation
+sudo curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
+
 
 logger "blue" "Installing RackHD Docker-machine driver"
-curl -L https://github.com/emccode/docker-machine-rackhd/releases/download/v0.1.0/docker-machine-driver-rackhd.`uname -s`-`uname -m` > /usr/local/bin/docker-machine-driver-rackhd && chmod +x /usr/local/bin/docker-machine-driver-rackhd
+sudo curl -L https://github.com/emccode/docker-machine-rackhd/releases/download/v0.1.0/docker-machine-driver-rackhd.`uname -s`-`uname -m` > /usr/local/bin/docker-machine-driver-rackhd && chmod +x /usr/local/bin/docker-machine-driver-rackhd
 
